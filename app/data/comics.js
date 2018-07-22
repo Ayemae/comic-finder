e17af3fe1e6bf7c15bf6a99ac87ecb7e31df1eab
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-07-22 00:57:17 -0400

    ...

---
+++ app/data/comics.js
@@ -0,0 +1,152 @@
+{
+    "title": "6-Commando",
+    "url": "http://www.viciousprint.com/6commando/",
+    "scores" : [
+        1,
+        3,
+        2,
+        3,
+        3,
+        2,
+        3,
+        4,
+        3,
+        -
+    ]
+}
+
+{
+    "title": "Chirault",
+    "url": "http://chirault.sevensmith.net/",
+    "scores" : [
+        1,
+        3,
+        2,
+        3,
+        3,
+        2,
+        3,
+        4,
+        3,
+        -
+    ]
+}
+
+{
+    "title": "Damsels Don't Wear Glasses",
+    "url": "http://damsels-dont-wear-glasses.com/",
+    "scores" : [
+        1,
+        4,
+        3,
+        2,
+        4,
+        1,
+        4,
+        5,
+        3,
+        -
+    ]
+}
+
+{
+    "title": "Heart of Keol",
+    "url": "http://www.heartofkeol.com/",
+    "scores" : [
+        1,
+        2,
+        3,
+        4,
+        2,
+        3,
+        3,
+        5,
+        3,
+        -
+    ]
+}
+
+{
+    "title": "Lapse",
+    "url": "http://www.lapsecomic.com/",
+    "scores" : [
+        1,
+        2,
+        1,
+        4,
+        2,
+        1,
+        2,
+        3,
+        3,
+        2
+    ]
+}
+
+{
+    "title": "Michael",
+    "url": "http://michaelcomic.com/",
+    "scores" : [
+        1,
+        3,
+        2,
+        2,
+        2,
+        2,
+        2,
+        4,
+        3,
+        -
+    ]
+}
+
+{
+    "title": "Seamus and Abbie",
+    "url": "http://sombulus.com/",
+    "scores" : [
+        2,
+        2,
+        2,
+        5,
+        4,
+        3,
+        2,
+        5,
+        3,
+        -
+    ]
+}
+
+{
+    "title": "Sombulus",
+    "url": "http://sombulus.com/",
+    "scores" : [
+        2,
+        5,
+        3,
+        3,
+        2,
+        3,
+        1,
+        5,
+        3,
+        4
+    ]
+}
+
+{
+    "title": "Suihira: The City of Water",
+    "url": "http://www.suihira.com/,
+    "scores" : [
+        3,
+        3,
+        2,
+        3,
+        4,
+        4,
+        3,
+        4,
+        2,
+        3
+    ]
+}
